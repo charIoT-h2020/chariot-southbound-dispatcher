632def1d76c2170021bba55b28867b7949722522
George Theofilis <g.theofilis@clmsuk.com>
2018-09-20 16:48:01 +0300

    Add demo message producer

---
+++ chariot_log_storage/send_log.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+
+mqtt.Client.connected_flag = False
+
+
+def on_message(client, userdata, message):
+    print("(%s) message received(%s): %s  " % (message.topic, message.retain, message.payload.decode("utf-8")))
+    if message.retain == 1:
+        print("This is a retained message")
+
+
+def on_log(client, userdata, level, buf):
+    print("log: ", buf)
+
+
+# Initialize connection to southbound
+broker = '172.18.1.2'
+consumer = mqtt.Client('chariot_log_storage')
+consumer.connect(broker)
+
+consumer.on_log = on_log
+consumer.on_message = on_message
+
+consumer.publish('storage/urn:ngsi-ld:temp:001', '0')
