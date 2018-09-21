5e6420d58db8370ffdbab9c233e6142ec79010cb
George Theofilis <g.theofilis@clmsuk.com>
2018-09-21 11:00:17 +0300

    Use correct topic name

--- chariot_log_storage/send_log.py
+++ chariot_log_storage/send_log.py
@@ -3,7 +3,9 @@ from consumer import Client
 
 
 class MessageGenerator(Client):
-    pass
+    @staticmethod
+    def on_log(client, userdata, level, buf):
+        print("log: ", buf)
 
 
 # Initialize connection to southbound
@@ -11,5 +13,5 @@ broker = '172.18.1.2'
 client_id = 'chariot_log_storage_demo'
 
 producer = MessageGenerator(client_id, broker)
-producer.publish('storage/urn:ngsi-ld:temp:001', '0')
+producer.publish('dispatcher/urn:ngsi-ld:temp:001', '0')
 producer.start(False)
