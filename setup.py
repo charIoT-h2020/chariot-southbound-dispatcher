86e4e1e7ca98f93d5f92a925334927a46973511c
George Theofilis <g.theofilis@clmsuk.com>
2019-02-26 11:13:48 +0000

    Bump version: 0.4.5 → 0.4.6

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot_southbound_dispatcher',
-    version='0.4.5',
+    version='0.4.6',
     zip_safe=False,
 )
